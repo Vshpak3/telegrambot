2a05f368e0a2b4cb282e2921a510600a2dd79ddd
vadym <vadimshpak3@gmail.ocm>
2025-05-06 02:07:53 +0900

    update hostname

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 const { createServer } = require('node:http');
-// const hostname = '127.0.0.1';
+const hostname = 'https://botfinal-901c3f6e3f3a.herokuapp.com/';
 const port = 3000;
 const server = createServer((req, res) => {
   res.statusCode = 200;
@@ -12,7 +12,7 @@ const server = createServer((req, res) => {
   res.end('Hello World');
 });
 
-server.listen(port, () => {
+server.listen(port, hostname, () => {
   console.log(`Server running `);
 
   function sendText() {
